402b0a799b9c698cbebb6f0242f2411dd8f70476
trurrdgr <100024417+trurrdgr@users.noreply.github.com>
2022-05-20 07:51:08 +0530

    Update GCngrok.sh

--- GC/GCngrok.sh
+++ GC/GCngrok.sh
@@ -4,4 +4,4 @@ echo "Download ngrok"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-./ngrok authtoken 29NkmnfalCpw3O2UGu752AVwMr8_77kPqtRjB14sK6d731jrD
+./ngrok authtoken "NGROK_AUtoken"
